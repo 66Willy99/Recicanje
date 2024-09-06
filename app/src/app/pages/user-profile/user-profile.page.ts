36cdc61a3ca83e37b5d004abe9bd6c8292b576cc
Jeremy <jeremyvillanuevam@gmail.com>
2024-09-06 16:02:21 -0400

    User page create

--- app/src/app/pages/user-profile/user-profile.page.ts
+++ app/src/app/pages/user-profile/user-profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-user-profile',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserProfilePage implements OnInit {
 
-  constructor() { }
+  user: string = "";
+
+  constructor(private navCtrl: NavController, private toastController: ToastController) { }
 
   ngOnInit() {
+  }
+
+  async MensajeLogin(mensaje: string, color: string) {
+    const toast = await this.toastController.create({
+      message: mensaje,
+      duration: 2000,  
+      position: 'top', 
+      color: color    
+    });
+    toast.present();
+  }
+
+  ActName(){
+
+    if(this.user.trim() == ""){
+      this.MensajeLogin('Debes ingresar un nombre para continuar','danger');
+    }
+    else if(this.user.length > 15){
+      this.MensajeLogin('Maximo de 15 Caracteres','danger');
+    }
+    else{
+      this.MensajeLogin('Nombre actualizado correctamente','success');
+      this.user = this.user;
+    }
+
+  }
+
+  Cierre(){
+    this.navCtrl.navigateForward('/login');
   }
 
 }
