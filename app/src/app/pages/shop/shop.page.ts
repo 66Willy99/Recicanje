a685147a0475ba5fe59ebff11a0551184853e640
Willy <guillermo.mercado26@gmail.com>
2024-09-06 00:37:24 -0400

    fixed shop page

--- app/src/app/pages/shop/shop.page.ts
+++ app/src/app/pages/shop/shop.page.ts
@@ -13,42 +13,42 @@ export class ShopPage implements OnInit {
       id: '1',
       name: 'cafe',
       stock: 5,
-      imageUrl: '',
+      imageUrl: 'https://tofuu.getjusto.com/orioneat-local/resized2/PnS4KYYj75HL5co8A-1200-1200.webp',
       price: 200
     },
     {
       id: '2',
       name: 'dona',
       stock: 5,
-      imageUrl: '',
+      imageUrl: 'https://buenprovecho.hn/wp-content/uploads/2019/06/iStock-624745020-1.jpg',
       price: 400
     },
     {
       id: '3',
       name: 'empanada',
       stock: 5,
-      imageUrl: '',
+      imageUrl: 'https://lanuevamendez.cl/wp-content/uploads/2021/06/IMG_0153.jpeg',
       price: 500
     },
     {
       id: '4',
       name: 'muffin',
       stock: 5,
-      imageUrl: '',
+      imageUrl: 'https://bakingwithbutter.com/wp-content/uploads/2022/08/6-blueberry-muffins-1-720x720.jpg',
       price: 300
     },
     {
       id: '5',
       name: 'medialuna',
       stock: 5,
-      imageUrl: '',
+      imageUrl: 'https://assets.elgourmet.com/wp-content/uploads/2011/09/shutterstock_1366373012-1024x683.jpg.webp',
       price: 500
     },
     {
       id: '6',
       name: 'hallula',
       stock: 5,
-      imageUrl: '',
+      imageUrl: 'https://dailyfresh.cl/cdn/shop/files/hallullajamonqueso.jpg?v=1700499925&width=1445',
       price: 600
     }
   ]
