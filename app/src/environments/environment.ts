ec3251bfed31ccd2f1cb246b4e56dfee658f7153
Lunasterio <143145540+Lunasterio@users.noreply.github.com>
2024-09-24 21:11:21 -0300

    Update environment.ts

--- app/src/environments/environment.ts
+++ app/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBqG3iENZ0egJAboAgTfIm2toPHmqPz-Yo",
+    authDomain: "recicanje-app.firebaseapp.com",
+    projectId: "recicanje-app",
+    storageBucket: "recicanje-app.appspot.com",
+    messagingSenderId: "40075162777",
+    appId: "1:40075162777:web:a52a40a9c1e14001297408",
+    measurementId: "G-VQ1RK2N747"
+  },
 };
 
 /*
