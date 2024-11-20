b3430535956d806565902a8d71edee1d9d4aa35b
Jeremy <jeremyvillanuevam@gmail.com>
2024-11-19 21:37:10 -0300

    Creacion de Mock

--- app/src/app/services/auth-user.service.spec.ts
+++ app/src/app/services/auth-user.service.spec.ts
@@ -1,13 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-
-import { AuthUserService } from './auth-user.service';
+import { of } from 'rxjs';
+import { AuthService } from './auth-user.service';
 
 describe('AuthUserService', () => {
-  let service: AuthUserService;
+  let service: AuthService;
+  let AuthMock: any;
 
   beforeEach(() => {
+
+    AuthMock = jasmine.createSpyObj('AuthService', ['collection', 'createId']);
+    AuthMock.createId.and.returnValue('mockedId');
+
+    AuthMock.collection.and.returnValue({
+      doc: jasmine.createSpy().and.returnValue({
+        set: jasmine.createSpy().and.returnValue(Promise.resolve()),
+        update: jasmine.createSpy().and.returnValue(Promise.resolve()),
+        delete: jasmine.createSpy().and.returnValue(Promise.resolve())
+      }),
+      valueChanges: jasmine.createSpy().and.returnValue(of([]))
+    });
+
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthUserService);
+    service = TestBed.inject(AuthService);
   });
 
   it('should be created', () => {
